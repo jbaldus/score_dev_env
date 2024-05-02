ad2e46f0dfb538b21f2def1c1b22eca7dd54b66b
Jason Baldus <jason.baldus@gmail.com>
2024-05-02 13:09:42 -0400

    Does this score.sh work?

--- score.sh
+++ score.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-python3 -m venv venv
-source venv/bin/activate
-pip install -r requirements.txt
-python score.py
+DIR=/tmp/score
+mkdir -p $DIR
+GH="https://raw.githubusercontent.com/jbaldus/score_dev_env/main"
+
+pushd $DIR 2>/dev/null
+if [[ ! -e venv ]]; then
+    python3 -m venv venv
+    wget "$GH/requirements.txt" > /dev/null 2>&1
+    source $DIR/venv/bin/activate
+    pip install -r $DIR/requirements.txt
+fi
+source $DIR/venv/bin/activate
+wget "$GH/score.py" > /dev/null 2>&1
+python $DIR/score.py
